7544646969864cf83036e489ce69656f771d45a0
Elimane Fofana <elimane.fofana@supinfo.com>
2021-10-18 03:17:22 +0000

    first commit

---
+++ src/main/java/com/subscription/handler/subscriptionHandler/SubscriptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.subscription.handler.subscriptionHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subscription/handler/subscriptionHandler/SubscriptionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subscription.handler.subscriptionHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
