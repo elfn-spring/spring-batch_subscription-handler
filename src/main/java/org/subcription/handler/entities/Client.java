992be88af91fb636dad4fe86acd0c59ab3ac343b
Elimane Fofana <elimane.fofana@supinfo.com>
2021-10-18 03:34:44 +0000

    new

---
+++ src/main/java/org/subcription/handler/entities/Client.java
@@ -0,0 +1,8 @@
+package org.subcription.handler.entities;
+
+/**
+ * @PROJECT subscriptionHandler
+ * @Author Elimane on 18/10/2021
+ */
+public class Client {
+}
---
+++ src/main/java/org/subcription/handler/entities/State.java
@@ -0,0 +1,9 @@
+package org.subcription.handler.entities;
+
+/**
+ * @PROJECT subscriptionHandler
+ * @Author Elimane on 18/10/2021
+ */
+public enum State {
+  EXPIRED, VALID, ALMOST_EXPIRED
+}
